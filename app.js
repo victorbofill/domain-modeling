ee70e1c68bb6ed3d26d10e031d80de708336f6b3
Victor Bofill <terranvas@gmail.com>
2018-02-04 18:10:54 -0800

    Done with article.

--- app.js
+++ app.js
@@ -1,2 +1,41 @@
 'use strict';
 
+var EpicFailVideo = function(epicRating, hasAnimals) {
+  this.epicRating = epicRating;
+  this.hasAnimals = hasAnimals;
+}
+
+EpicFailVideo.prototype.generateRandom = function(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+EpicFailVideo.prototype.dailyLikes = function() {
+  var viewers, percentage;
+
+  viewers = this.generateRandom(10, 30) * this.epicRating;
+
+  if (this.hasAnimals) {
+    percentage = 0.75;
+  } else {
+    percentage = 0.40
+  }
+
+  return Math.round(viewers * percentage);
+}
+
+EpicFailVideo.prototype.weeklyLikes = function() {
+  var total = 0;
+
+  for (var i = 0; i < 7; i++) {
+    total += this.dailyLikes();
+  }
+
+  return total;
+}
+
+var parkourFail = new EpicFailVideo(7, false);
+var corgiFail = new EpicFailVideo(4, true);
+
+console.log(parkourFail.weeklyLikes());
+console.log(corgiFail.weeklyLikes());
+
